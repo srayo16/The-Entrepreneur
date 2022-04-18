088c5df63efd12c160ca6db11f5f105de1a53ba3
srayo16 <srayosikder@gmail.com>
2022-04-18 23:12:35 +0600

    last time edited

--- src/Pages/Banner/Banner.js
+++ src/Pages/Banner/Banner.js
@@ -14,39 +14,39 @@ const Banner = () => {
     return (
         <Carousel activeIndex={index} onSelect={handleSelect} className='overflow-hidden'>
             <Carousel.Item>
-                <img 
+                <img
                     className="d-block img-fluid w-100 handleImage"
-                    src= {banner1}
+                    src={banner1}
                     alt="First slide"
                 />
                 <Carousel.Caption className='handleBg rounded'>
                     <h3 className='handleTitle'>The Owner of Udvash</h3>
-                    <p className='handleDescrip mt-3'>This is Mahmudul Hasan Sohag.On 2000 year, He established a education institute name 'Udvash-Unmesh'</p>
+                    <p className='handleDescrip mt-3'>This is Mahmudul Hasan Sohag.On 2000 year, He established a education institute name 'Udvash-Unmesh'.</p>
                 </Carousel.Caption>
             </Carousel.Item>
             <Carousel.Item>
                 <img
                     className="d-block w-100 img-fluid handleImage"
-                    src= {banner2}
+                    src={banner2}
                     alt="Second slide"
                 />
 
                 <Carousel.Caption className='handleBg rounded'>
-                    <h3 className='handleTitle'>The discover of EVM Machine</h3>
+                    <h3 className='handleTitle'>The Discoverer of EVM Machine</h3>
                     <p className='handleDescrip mt-3'>Mahmudul Hasan Sohag discoverd EVM Machine for his country for first time.</p>
                 </Carousel.Caption>
             </Carousel.Item>
             <Carousel.Item>
                 <img
                     className="d-block w-100 img-fluid handleImage"
-                    src= {banner3}
+                    src={banner3}
                     alt="Third slide"
                 />
 
                 <Carousel.Caption className='handleBg rounded'>
                     <h3 className='handleTitle'>The Owner of Rokmari</h3>
                     <p className='handleDescrip mt-3'>
-                    On 19 January 2012, Mahmudul Hasan Sohag opening a online bookseller institute name 'Rokmari'
+                        On 19 January 2012, Mahmudul Hasan Sohag opening a online bookseller institute name 'Rokmari'.
                     </p>
                 </Carousel.Caption>
             </Carousel.Item>
