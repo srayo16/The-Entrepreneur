4a3666b950801fcdebdfc02600063f62ffb4c3c5
srayo16 <srayosikder@gmail.com>
2022-04-26 15:07:54 +0600

    extra

--- src/Pages/About/About.js
+++ src/Pages/About/About.js
@@ -6,6 +6,7 @@ const About = () => {
         <div className='container d-flex justify-content-center align-items-center mt-1 handleFirstParent' style={{height: '800px'}}>
             <div className='w-75 pe-5' >
                 <h1 className='mb-3 text-danger'>The goal of life</h1>
+                <small className='fw-bolder'>নামঃ মো সায়মন শিকদার শ্রেয়।</small>
                 <p>সালাফদের যোগ্য উত্তরসূরী হয়ে গড়ে উঠাই জীবনের মূল প্রধান লক্ষ্য। অতঃপর দুনিয়ার খাতিরে কিছু লক্ষ্য সবারই থাকা অপরিহার্য। আমার ইচ্ছা প্রথমে ফুল স্টাক ওয়েব ডেভলপার হওয়া। এরপর সম্ভব হলে সফটওয়্যার ইঞ্জিনিয়ার এর দিকে ধাবিত হওয়া। এর জন্য পরিশ্রম করতে সর্বদা মানসিকভাবে প্রস্তুত থাকি ও চেষ্টা করি। আল্ল-হ সুবহানাহু ওয়াতাআ'লার রহমত ও সাহায্য  থাকলে তা একদিন পূরণ হবেই হবে ইন শা আল্ল-হ।
                 </p>
             </div>
--- src/Pages/FirstSection/FirstSection.js
+++ src/Pages/FirstSection/FirstSection.js
@@ -2,28 +2,28 @@ import React, { useEffect, useState } from 'react';
 import './FirstSection.css';
 import person from '../../Images/Person/Mahmudul-hasan.jpg';
 const FirstSection = () => {
-    const [description , setDescription] = useState([]);
-    const [expand , setExpand] = useState(false);
-    useEffect(() =>{
+    const [description, setDescription] = useState([]);
+    const [expand, setExpand] = useState(false);
+    useEffect(() => {
         fetch('person.json')
-        .then(res => res.json())
-        .then(data => setDescription(data))
-    } , [])
+            .then(res => res.json())
+            .then(data => setDescription(data))
+    }, [])
 
     return (
         <div className='container d-flex justify-content-center align-items-center mt-5 pt-5 handleFirstParent'>
             <div className='w-75 pe-5' >
                 <h1>The Life Stroy of<br /><span className='text-danger'>Mahmudul Hasan Sohag</span></h1>
                 {
-                    description.map(item => <p item ={item} key={item.id} className='pt-3'> 
-                    {!expand &&  item.description.slice(0,1000) + '...'} {!expand && <button className='btn btn-light fw-bold text-primary m-0 p-0' onClick={() => setExpand(true)}>Read more</button>}
-                    {expand && item.description} {expand && <button className='btn btn-light fw-bold text-primary m-0 p-0' onClick={() => setExpand(false)}>Read less</button>}
-                     </p> )
+                    description.map(item => <p item={item} key={item.id} className='pt-3'>
+                        {!expand && item.description.slice(0, 1000) + '...'} {!expand && <button className='btn btn-light fw-bold text-primary m-0 p-0' onClick={() => setExpand(true)}>Read more</button>}
+                        {expand && item.description} {expand && <button className='btn btn-light fw-bold text-primary m-0 p-0' onClick={() => setExpand(false)}>Read less</button>}
+                    </p>)
                 }
-                
+
             </div>
             <div>
-                <img src={person}  alt="" />
+                <img src={person} alt="" />
             </div>
         </div>
     );
